cde9f6b532287e7802ea7b7832d266940c93ca90
Maksim <pumpbro@gmail.com>
2024-03-26 11:48:29 +0300

    feat: create axios api tool

--- frontend_app/src/main.js
+++ frontend_app/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import App from './App'
+import axios from 'axios'
+const app = createApp(App)
 
-createApp(App).use(store).use(router).mount('#app')
+app.use(axios)
+app.mount('#app')
