6ac4d0ffacc1ae5b4995b462c19dcdf681ffad60
Maksim <pumpbro@gmail.com>
2024-04-04 15:59:49 +0300

    feat: make page for search item

--- frontend_app/src/views/SingleItem.vue
+++ frontend_app/src/views/SingleItem.vue
@@ -1,37 +1,14 @@
 <template>
-    <item-filter></item-filter>
-    <item-card v-bind:item="item"/>
+    <item-card></item-card>
 </template>
 
 <script>
-import axios from 'axios'
-
 import ItemCard from '@/components/ItemCard'
-import ItemFilter from '@/components/ItemFilter'
 
 export default {
     components: {
         ItemCard,
-        ItemFilter,
     },
-    data () {
-        return {
-            item: {
-                id: 0,
-                name: 'empty',
-                price: 0,
-                quantity: 0,
-                active: false,
-            },
-        }
-    },
-    methods: {
-        async getItem() {
-            axios.get("http://localhost:2000/items/{id}").then((response) => {
-                this.item = response.data['item']
-            })
-        }
-    }
 }
 
 
